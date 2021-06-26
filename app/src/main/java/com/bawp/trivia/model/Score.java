1c3a7f747b5c0a64b3f6f239e56ec45f5754545c
mohd ahad <ahadmohd407>
2021-06-26 16:10:57 +0530

    Initial commit

---
+++ app/src/main/java/com/bawp/trivia/model/Score.java
@@ -0,0 +1,19 @@
+package com.bawp.trivia.model;
+/**
+ * @Author= Mohd Ahad
+ */
+public class Score {
+
+    private int score;
+
+    public Score() {
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
